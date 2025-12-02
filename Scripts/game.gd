327f7a742a051bb7a53557300c306d929243156b
AccidentByDragon <karl17loe@hotmail.com>
2025-12-02 11:55:43 +0100

    rewritten spawn loop
    rewrote wave spawning loop and renamed the groups inorder to reuse the loop and simplify the strings used to find the groups: ie. west for group name rather than westspawns allows me to simply use west everywhere

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -22,7 +22,7 @@ func position_to_next_wave():
 	if current_nodes == starting_nodes:
 		currentWave += 1		
 		await get_tree().create_timer(0.5).timeout
-		prepare_spawn("north", 2.0, 1.0) #Origin of waves, Number of "waves",  time between waves
+		prepare_spawn("east", 2.0, 1.0) #Origin of waves, Number of "waves",  time between waves
 		print(currentWave)
 
 func prepare_spawn(origin, multiplier, timeBetween):
@@ -31,20 +31,7 @@ func prepare_spawn(origin, multiplier, timeBetween):
 	print("wave_amount: ", wave_amount, "wave_wait_time: ", wave_wait_time)
 	spawn_wave(wave_amount, origin, wave_wait_time)
 
-func spawn_wave(amount, spawn_point, wait_between_waves):
-	if spawn_point == "north":
-		wavespawnloop("NorthSpawns", amount, wait_between_waves)
-		# this orignally would go through the spawn point swithout a for loop so would have each spawnpoint have its own variable
-		# and then spawn a wave add it trhough its own variable, i replaced this with a for loop that utilised get_nodes_in_group to make a cleaner code
-		# that achieved the same effect.
-	if spawn_point == "south":
-		wavespawnloop("SouthSpawns", amount, wait_between_waves)
-	if spawn_point == "west":
-		wavespawnloop("WestSpawns", amount, wait_between_waves)
-	if spawn_point == "east":
-		wavespawnloop("EastSpawns", amount, wait_between_waves)
-
-func wavespawnloop(origin, amount, wave_timer):
+func spawn_wave(amount, origin, wait_between_waves):
 	var spawns = get_tree().get_nodes_in_group(origin)
 	var current_wave_number: int = 1 
 	for i in amount:
@@ -53,11 +40,17 @@ func wavespawnloop(origin, amount, wave_timer):
 			var new_wave = wave.instantiate()
 			new_wave.global_position = point.global_position
 			add_child(new_wave)
+			new_wave.set_direction(origin)
 			print("Spawning wave at: ", point.name, " wave number: ", current_wave_number)
 			if point == spawns.back():
 				break
 		amount -= 1
-		await get_tree().create_timer(wave_timer).timeout
+		await get_tree().create_timer(wait_between_waves).timeout
+		## this orignally would go through the spawn point swithout a for loop so would have each spawnpoint have its own variable
+		## and then spawn a wave add it trhough its own variable, i replaced this with a for loop that utilised get_nodes_in_group to make a cleaner code
+		## that achieved the same effect.
+
+
 
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
--- Scripts/wave_logic.gd
+++ Scripts/wave_logic.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 class_name waveThreat
-enum {NORTH, EAST, SOUTH, WEST}
+enum Direction {north, east, south, west}
+var current_direction: String
 
 const speed: float = 250.0
 
@@ -9,4 +10,23 @@ var collided_with_player: bool = false
 var dir: Vector2
 
 func _process(delta):
-	pass
+	move(delta)
+	move_and_slide()
+	#print(current_direction)
+func move(delta):
+	velocity += dir * speed * delta
+	
+func set_direction(direction_string):
+	match direction_string:
+		"north":
+			dir.y = 1
+			current_direction = direction_string
+		"east":
+			dir.x = -1
+			current_direction = direction_string
+		"south":
+			dir.y = -1
+			current_direction = direction_string
+		"west":
+			dir.x = 1
+			current_direction = direction_string
