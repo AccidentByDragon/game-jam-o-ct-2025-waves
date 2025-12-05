f22fa9e49d866e34e643874729a91b3954adf37b
AccidentByDragon <karl17loe@hotmail.com>
2025-12-05 15:11:43 +0100

    Update boat.gd

--- Scripts/boat.gd
+++ Scripts/boat.gd
@@ -6,7 +6,7 @@ extends CharacterBody2D
 @export var rotation_speed = 1.5
 var curent_speed: float
 var rotation_direction = 0
-const collision_force: float = 500.0
+const collision_force: float = 250.0
 var player_health = 100
 var is_knocked_back: bool = false
 
@@ -24,16 +24,18 @@ func _physics_process(delta: float) -> void:
 		curent_speed -= 50.0
 		if curent_speed < -200:
 			curent_speed = -200
-	curent_speed -= deceleration_speed * delta
-	if curent_speed < 0:
-		curent_speed = 0
+	else:
+		curent_speed -= deceleration_speed * delta
+		if curent_speed < 0:
+			curent_speed = 0
+			is_knocked_back = false # this solved our issue as it gives us a chnace to disable the shove
 	if is_knocked_back == true:
 		for index in get_slide_collision_count():
 			var collision: KinematicCollision2D = get_slide_collision(index)
 			if collision.get_collider().name.contains("Wave"):
-				velocity = collision.get_normal()*curent_speed # this combined with how we disable colisions is a problem
+				velocity = collision.get_normal()*curent_speed
 	elif is_knocked_back != true:
-		velocity = transform.x * curent_speed # the transform.x is causing us issues with knockback
+		velocity = transform.x * curent_speed
 		rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
 
@@ -41,15 +43,13 @@ func _physics_process(delta: float) -> void:
 func _on_wave_collision_detection_body_entered(body: Node2D) -> void:
 	if body.name.contains("Rock"):
 		print("player has hit ", body.name)
-		player_health = player_health -10
+		player_health = player_health -25
 		print(player_health)
 	elif body.name.contains("Wave"):
 		print("a wave has hit player")
+		player_health = player_health -10
 		curent_speed = collision_force
-		if curent_speed <= 0:
-			is_knocked_back = false
-		else:
-			is_knocked_back = true
+		is_knocked_back = true
 		#var knockback_direction = body.position.direction_to(self.position)
 		#print(knockback_direction)
 		#velocity = knockback_direction * collision_force # this telports the player rather than sliding them
