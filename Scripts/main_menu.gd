6175c359db45e28c6963755ef700ba90d5643489
AccidentByDragon <karl17loe@hotmail.com>
2025-12-07 17:42:03 +0100

    help menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -4,6 +4,8 @@ extends Control
 func _on_start_game_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 
+func _on_help_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/help_menu.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
